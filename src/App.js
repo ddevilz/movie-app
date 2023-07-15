9e437894fd62b895e206d109258ca13aa4e67d35
Devashish jadhav <jadhavom24@gmail.com>
2023-07-16 00:58:25 +0530

    api added & tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
 
-const App = () => {
+const App = () => { 
+  console.log(process.env.REACT_APP_TMDB_TOKEN)
+  useEffect(() => {
+    apiTest()
+  }, [])
+
+  const apiTest = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+        console.log(res)
+      })
+  }
   return (
     <div>
-      
+      <h1>Hello</h1>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './app/store';
+import store from './app/store.js';
 import { Provider } from 'react-redux';
 
 
