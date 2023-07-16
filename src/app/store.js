723fb1704337060c9a55e3ea44b3a160abca2a29
Devashish jadhav <jadhavom24@gmail.com>
2023-07-16 17:59:02 +0530

    Hero section added

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import homeSlice from "../features/homeSlice";
 
 export default configureStore({
-  reducer: {}
-})
+  reducer: {
+    home: homeSlice,
+  },
+});
