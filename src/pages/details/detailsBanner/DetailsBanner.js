1582efd087fdf5580e1999d90c389fe810455239
Devashish jadhav <jadhavom24@gmail.com>
2023-07-23 22:43:51 +0530

    details banner update

--- src/pages/details/detailsBanner/DetailsBanner.js
+++ src/pages/details/detailsBanner/DetailsBanner.js
@@ -16,6 +16,10 @@ const DetailsBanner = ({ video, crew }) => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
+  const { url } = useSelector((state) => state.home);
+
+  const _genres = data?.genres?.map((g) => g.id);
+
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
@@ -25,7 +29,44 @@ const DetailsBanner = ({ video, crew }) => {
   return (
     <div className="detailsBanner">
       {!loading ? (
-        <div>Details Content...</div>
+        <>
+          {!!data && (
+            <React.Fragment>
+              <div className="backdrop-img">
+                <Img src={url.backdrop + data.backdrop_path} />
+              </div>
+              <div className="opacity-layer"></div>
+              <ContentWrapper>
+                <div className="content">
+                  <div className="left">
+                    {data.poster_path ? (
+                      <Img
+                        className="posterImg"
+                        src={url.backdrop + data.poster_path}
+                      />
+                    ) : (
+                      <Img className="posterImg" src={PosterFallback} />
+                    )}
+                  </div>
+                  <div className="right">
+                    <div className="title">
+                      {`${data.name || data.title} (${dayjs(
+                        data.release_date
+                      ).format("YYYY")})`}
+                    </div>
+                    <div className="subtitle">{data.tagline}</div>
+
+                    <Genres data={_genres} />
+
+                    <div className="row">
+                      <CircleRating rating={data.vote_average.toFixed(1)} />
+                    </div>
+                  </div>
+                </div>
+              </ContentWrapper>
+            </React.Fragment>
+          )}
+        </>
       ) : (
         <div className="detailsBannerSkeleton">
           <ContentWrapper>
