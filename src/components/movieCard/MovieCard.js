8518294ad3ce805278fe66c78bfd43d61a46d83c
Devashish jadhav <jadhavom24@gmail.com>
2023-07-25 19:18:43 +0530

    explore page added

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -27,7 +27,7 @@ const MovieCard = ({ data, fromSearch, mediaType }) => {
                 {!fromSearch && (
                     <React.Fragment>
                         <CircleRating rating={data.vote_average.toFixed(1)} />
-                        <Genres data={data.genre_ids.slice(0, 2)} />
+                        <Genres data={data?.genre_ids?.slice(0, 2)} />
                     </React.Fragment>
                 )}
             </div>
--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,13 +1,15 @@
-import React from 'react'
 import "./style.css";
-
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
   return (
-    <div>
-      
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
